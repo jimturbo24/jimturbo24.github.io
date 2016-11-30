aa9c01ce5f66e47e42947d4db43bab6956bdabad
jimturbo24 <jim.trbovich@gmail.com>
2016-11-29 23:28:07 -0600

    added work section

--- jekyll_site/assets/js/functions.js
+++ jekyll_site/assets/js/functions.js
@@ -1,5 +1,49 @@
-$( document ).ready(function() {
-
-  // Get started!
+$(function() {
+	smoothScroll(1000);
+	workBelt();
+	workLoad();
 
 });
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+function workBelt(){
+
+	$('.thumb-unit').click(function(){
+			$('.work-slider').css('left','-100%');
+			$('.work-container').show();
+	});
+
+	$('.work-return').click(function(){
+			$('.work-slider').css('left',' 0%');
+			$('.work-container').hide(800);
+	});
+}
+
+function workLoad(){
+	$.ajaxSetup({ cache: false });
+
+	$('.thumb-unit').click(function() {
+		var $this = $(this)
+		var newTitle = $this.find('strong').text();
+		var newFolder = $this.data('folder');
+		var spinner = '<div class="loader">Loading...</div>';
+		var newHTML = '/work/' + newFolder + '.html';
+
+		$('.project-load').html(spinner).load(newHTML);
+		$('.project-title').text(newTitle);
+
+	});
+}
